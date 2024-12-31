c57d2c38cbbb8ba17abf65502cde4b776f1f5891
Roberto Butti <roberto.butti@gmail.com>
2024-12-31 15:01:37 +0100

    Adding tests

---
+++ tests/Unit/StoryblokUtilsTest.php
@@ -0,0 +1,23 @@
+<?php
+
+
+declare(strict_types=1);
+
+use Roberto\Storyblok\Mapi\StoryblokUtils;
+
+test('Testing getRegionFromSpaceId', function ($spaceId, $region): void {
+    expect(StoryblokUtils::getRegionFromSpaceId($spaceId))->toBe($region);
+})->with([
+    ['1000', 'EU'],
+    ['10000', 'EU'],
+    [100_000_000, 'US'],
+]);
+
+test('Testing baseUriFromRegionForMapi', function ($region, $baseUri): void {
+    expect(StoryblokUtils::baseUriFromRegionForMapi($region))->toBe($baseUri);
+})->with([
+    ['EU', 'https://mapi.storyblok.com'],
+    ['US', 'https://api-us.storyblok.com'],
+    ['NOTVALID', 'https://mapi.storyblok.com'],
+
+]);
