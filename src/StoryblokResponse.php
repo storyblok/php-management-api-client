77186590d8c09cceff59e9c4c1a182a30710e398
Roberto Butti <roberto.butti@gmail.com>
2025-01-18 18:43:25 +0100

    returning proper type for total()

--- src/StoryblokResponse.php
+++ src/StoryblokResponse.php
@@ -70,14 +70,20 @@ class StoryblokResponse implements StoryblokResponseInterface
         return null;
     }
 
-    public function total(): mixed
+    public function getHeaderInt(string $headerName): int|null
     {
-        return $this->getHeader("total");
+        $value =  $this->getHeader($headerName);
+        return is_numeric($value) ? (int) $value : null;
     }
 
-    public function perPage(): mixed
+    public function total(): int|null
     {
-        return $this->getHeader("per-page");
+        return  $this->getHeaderInt("total");
+    }
+
+    public function perPage(): int|null
+    {
+        return $this->getHeaderInt("per-page");
     }
 
     public function getResponseStatusCode(): int
@@ -103,7 +109,21 @@ class StoryblokResponse implements StoryblokResponseInterface
 
         $data = $this->data();
         $message = $data->get("error");
-        return $message ?? "Unknown error, " . $this->getResponseStatusCode();
+        if ($message) {
+            return $message;
+        }
+
+        $message = match ($this->getResponseStatusCode()) {
+            400 => "Bad Request. Wrong format was sent (eg. XML instead of JSON).",
+            401 => "Unauthorized. No valid API key provided.",
+            403 => "Forbidden. Insufficient permissions.",
+            404 => "Not Found. The requested resource doesn't exist (perhaps due to not yet published content entries).",
+            422 => "Unprocessable Entity. The request cannot be processed because it is invalid, for example, due to a missing required parameter or a duplicate key.",
+            429 => "Too many requests. Too many requests hit the API too quickly. We recommend an exponential backoff (throttling) of your requests.",
+            500, 502, 503, 504 => "Server error. We are unable to process your request.",
+            default => "Unknown error",
+        };
+        return $this->getResponseStatusCode() . " - " . $message;
     }
 
 
