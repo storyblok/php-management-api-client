0ed18c26969521939511dcaf0ccafb0449aa9eda
Roberto Butti <roberto.butti@gmail.com>
2025-01-22 23:58:19 +0100

    Introducing all() method

--- src/Endpoints/StoryApi.php
+++ src/Endpoints/StoryApi.php
@@ -16,11 +16,58 @@ use Storyblok\Mapi\StoryblokResponseInterface;
  */
 class StoryApi extends EndpointSpace
 {
+    public function all(): \Generator
+    {
+        $pageNumber = 1;
+        $itemsPerPage = 5;
+        $totalPages = null;
+
+        do {
+
+            // Fetch the current page
+            $response = $this->page($pageNumber, $itemsPerPage);
+
+            if ($response->isOk()) {
+                // Print the total number of tags (only on the first iteration)
+                if ($totalPages === null) {
+                    //echo "Total Stories: " . $response->total() . PHP_EOL;
+                    // Calculate the total number of pages
+                    $totalPages = ceil($response->total() / $itemsPerPage);
+                }
+
+                /** @var StoriesData $stories */
+                $stories = $response->data();
+                foreach ($stories as $story) {
+                    yield $story;
+                }
+
+                // Move to the next page
+                ++$pageNumber;
+            } elseif ($response->getResponseStatusCode() === 429) {
+                echo "Rate limit reached. Retrying in 1 second..." . PHP_EOL;
+                sleep(1);
+                // Wait for 1 second before retrying
+            } else {
+                // Handle other exceptions
+                echo "An error occurred: " . $response->getErrorMessage() . PHP_EOL;
+                break; // Exit the loop on non-recoverable errors
+            }
+
+        } while ($pageNumber <= $totalPages);
+    }
+
     public function page(int $page = 1, int $perPage = 25): StoryblokResponseInterface
     {
+        $options = [
+            'query' => [
+                'page' => $page,
+                'per_page' => $perPage,
+            ],
+        ];
         return $this->makeRequest(
             "GET",
             '/v1/spaces/' . $this->spaceId . '/stories',
+            options: $options,
             dataClass: StoriesData::class,
         );
     }
