c8152f9ab5165308afe14687b30dd0ceb6ebe0fa
Roberto Butti <roberto.butti@gmail.com>
2026-01-23 23:29:39 +0100

    Improving Test Coverage

--- tests/Feature/WorkflowStageChangeApiTest.php
+++ tests/Feature/WorkflowStageChangeApiTest.php
@@ -7,7 +7,9 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Storyblok\ManagementApi\Data\WorkflowStageChange;
 use Storyblok\ManagementApi\Endpoints\WorkflowStageChangeApi;
+use Storyblok\ManagementApi\Exceptions\StoryblokFormatException;
 use Storyblok\ManagementApi\ManagementApiClient;
+use Symfony\Component\HttpClient\Exception\ClientException;
 use Symfony\Component\HttpClient\MockHttpClient;
 
 final class WorkflowStageChangeApiTest extends TestCase
@@ -15,32 +17,33 @@ final class WorkflowStageChangeApiTest extends TestCase
     public function testListOfWorkflowStageChanges(): void
     {
         $responses = [
-            $this->mockResponse('list-workflow-stage-changes', 200, []),
-            $this->mockResponse('list-workflow-stage-changes', 200, []),
+            $this->mockResponse("list-workflow-stage-changes", 200, []),
+            $this->mockResponse("list-workflow-stage-changes", 200, []),
         ];
 
         $client = new MockHttpClient($responses);
         $mapiClient = ManagementApiClient::initTest($client);
         $workflowStageChangeApi = new WorkflowStageChangeApi($mapiClient, 123);
 
-        $storyblokResponse = $workflowStageChangeApi->page(withStory: '12345');
+        $storyblokResponse = $workflowStageChangeApi->page(withStory: "12345");
         $url = $storyblokResponse->getLastCalledUrl();
 
         $this->assertMatchesRegularExpression('/.*with_story=12345.*$/', $url);
 
         $workflowStageChanges = $storyblokResponse->data();
-        $this->assertSame(2, $workflowStageChanges->howManyWorkflowStageChanges());
+        $this->assertSame(
+            2,
+            $workflowStageChanges->howManyWorkflowStageChanges(),
+        );
     }
 
     public function testCreatingWorkflowStageChange(): void
     {
-        $responses = [
-            $this->mockResponse('one-workflow-stage-change', 200),
-        ];
+        $responses = [$this->mockResponse("one-workflow-stage-change", 200)];
 
         $client = new MockHttpClient($responses);
         $mapiClient = ManagementApiClient::initTest($client);
-        $changeApi = new WorkflowStageChangeApi($mapiClient, '222');
+        $changeApi = new WorkflowStageChangeApi($mapiClient, "222");
 
         $workflowStageChange = new WorkflowStageChange();
         $workflowStageChange->setStoryAndStage(123, 321);
@@ -48,7 +51,72 @@ final class WorkflowStageChangeApiTest extends TestCase
         $storyblokResponse = $changeApi->create($workflowStageChange);
         $url = $storyblokResponse->getLastCalledUrl();
 
-        $this->assertMatchesRegularExpression('/.*workflow_stage_changes.*$/', $url);
+        $this->assertMatchesRegularExpression(
+            '/.*workflow_stage_changes.*$/',
+            $url,
+        );
         $this->assertTrue($storyblokResponse->isOk());
+        $storyblokResponse->data();
+    }
+
+    public function testEmptyPayload(): void
+    {
+        $responses = [$this->mockResponse("empty", 200, [])];
+
+        $client = new MockHttpClient($responses);
+        $mapiClient = ManagementApiClient::initTest($client);
+        $workflowStageChangeApi = new WorkflowStageChangeApi($mapiClient, 123);
+
+        $this->expectException(StoryblokFormatException::class);
+        $this->expectExceptionMessage(
+            "Expected 'workflow_stage_changes' in the response.",
+        );
+        $workflowStageChangeApi
+            ->page(withStory: "12345")
+            ->data();
+    }
+
+    public function testOneEmptyPayload(): void
+    {
+        $responses = [$this->mockResponse("empty", 200, [])];
+
+        $client = new MockHttpClient($responses);
+        $mapiClient = ManagementApiClient::initTest($client);
+        $workflowStageChangeApi = new WorkflowStageChangeApi($mapiClient, 123);
+
+        $this->expectException(StoryblokFormatException::class);
+        $this->expectExceptionMessage(
+            "Expected 'workflow_stage_change' in the response.",
+        );
+
+        $workflowStageChange = new WorkflowStageChange();
+        $workflowStageChange->setStoryAndStage(123, 321);
+
+        $storyblokResponse = $workflowStageChangeApi->create(
+            $workflowStageChange,
+        );
+        $storyblokResponse->data();
+    }
+
+    public function testErrorMessage(): void
+    {
+        $responses = [$this->mockResponse("error", 200, [])];
+
+        $client = new MockHttpClient($responses);
+        $mapiClient = ManagementApiClient::initTest($client);
+        $workflowStageChangeApi = new WorkflowStageChangeApi($mapiClient, 123);
+
+        $this->expectException(StoryblokFormatException::class);
+        $this->expectExceptionMessage(
+            "Expected 'workflow_stage_change' in the response. Error",
+        );
+
+        $workflowStageChange = new WorkflowStageChange();
+        $workflowStageChange->setStoryAndStage(123, 321);
+
+        $storyblokResponse = $workflowStageChangeApi->create(
+            $workflowStageChange,
+        );
+        $storyblokResponse->data();
     }
 }
